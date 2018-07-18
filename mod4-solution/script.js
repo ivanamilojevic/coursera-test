1d715dc51ab8b9a11eaf267e5c9d73def1607b80
ivanamilojevic <40363726+ivanamilojevic@users.noreply.github.com>
2018-07-18 09:00:34 -0400

    Add files via upload

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (window) {
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -58,14 +58,12 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter === 'J') {
-    console.log (byeSpeaker.speak + " " + names[i]);
-    }      
- else {
-    console.log (helloSpeaker.speak + " " + names[i]);
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+    }  else {
+    helloSpeaker.speak(names[i]);
     }
 }
 
-window.names = names; 
+})();
 
-})(window);

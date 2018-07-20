8b25283766f2532a5c6c96e1e68dc666ec7c6c12
ivanamilojevic <40363726+ivanamilojevic@users.noreply.github.com>
2018-07-20 16:23:30 -0400

    Add files via upload

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -117,11 +117,11 @@ function buildAndShowHomeHTML (categories) {
 //       // Hint: you need to surround the chosen category short name with something before inserting
 //       // it into the home html snippet.
 //       
-       var a = "'" + chosenCategoryShortName.short_name + "'";
-       
+       var x = "'" + chosenCategoryShortName.short_name + "'";
+
 //       // var homeHtmlToInsertIntoMainPage = ....
 
-       var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml,"randomCategoryShortName", a);
+       var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml,"randomCategoryShortName", x);
 
 //       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
 //       // Use the existing insertHtml function for that purpose. Look through this code for an example
